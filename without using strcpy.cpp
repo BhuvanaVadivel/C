4c175fe776c1e42755b4c4914a86c9fb1af3e339
BHUVANESHWARI V <118094379+BhuvanaVadivel@users.noreply.github.com>
2024-02-13 22:54:49 +0530

    Add files via upload

---
+++ without using strcpy.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[30]="bhuvan";
+	char s2[30];
+	int i;
+	//printf("%s",s1);
+	for(i=0;s1[i]!='\0';i++)
+	{
+		s2[i]=s1[i];
+	}
+	s2[i]!='\0';
+	printf("%s",s2);
+}
